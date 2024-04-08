48fbe06cb8b008877e0f21987d3183baf62148d1
LehmanM05 <163595143+LehmanM05@users.noreply.github.com>
2024-04-08 12:47:12 -0400

    Update 01_01_invitations_ch.sql

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -3,7 +3,6 @@
 -- Show their first name, last name, and email address.
 -- Sort the list of results by last name.
 
-SELECT * FROM FirstName;
-SELECT * FROM LastName;
-SELECT * FROM Email;
-ORDER BY LastName
+SELECT FirstName, LastName, Email
+FROM Customers
+ORDER BY LastName;
